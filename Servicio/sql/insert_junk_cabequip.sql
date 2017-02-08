70173f6647ac935a9ea81f5677a56ad53aaaaadb
jorge-crespo-cedeno <jorgecrespo@yahoo.com>
2017-02-08 12:53:24 -0500

    adding sql's

---
+++ Servicio/sql/insert_junk_cabequip.sql
@@ -0,0 +1,19 @@
+INSERT INTO cabequip (poste_id, rfid, operador, tipo, uso, activo, cable)
+VALUES  (104552, 'PIRATA01', 'CLARO', 'FIBRA', 'ACOMETIDA', 'S', 'S');
+
+INSERT INTO cabequip (poste_id, rfid, operador, tipo, uso, activo, cable)
+VALUES  (104552, 'PIRATA02', 'MOVISTAR', 'COBRE', 'ACOMETIDA', 'S', 'S');
+
+INSERT INTO cabequip (poste_id, rfid, operador, tipo, uso, activo, cable)
+VALUES  (104552, 'PIRATA03', 'PUNTONET', 'MULTIPAR', 'DISTRIBUCION', 'S', 'S');
+
+INSERT INTO cabequip (poste_id, rfid, operador, tipo, uso, activo, cable)
+VALUES  (104552, 'PIRATA04', 'WHATEVER', 'COBRE', 'ACOMETIDA', 'S', 'S');
+
+INSERT INTO cabequip (poste_id, rfid, operador, tipo, uso, activo, cable)
+VALUES  (97613, 'PIRATA05', 'CLARO', 'COBRE', 'DISTRIBUCION', 'S', 'S');
+
+INSERT INTO cabequip (poste_id, rfid, operador, tipo, uso, activo, cable)
+VALUES  (97613, 'PIRATA06', 'ELDECORREA', 'FIBRA', 'ACOMETIDA', 'S', 'S')
+/
+
